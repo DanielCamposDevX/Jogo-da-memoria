bdd9ce57210c3358e6b5edff5289ed31796750ed
DanielCamposDevX <daniel.campos.dev3@gmail.com>
2023-04-09 15:57:32 -0300

    Vesão com lista e limite de cartas

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-let i = 0;
-const cardo = document.querySelector('.cards');
-let carts = prompt('Quantas Cartas?');
-let resto = carts % 2;
-let acertos = 0;
+
+principal();
 
 function turncard(carta) {
     const carta1 = carta.querySelector(".l1");
@@ -15,11 +12,19 @@ function turncard(carta) {
 }
 
 
-
-if (resto == 0 && carts > 2 && carts < 15) {
-    while (i < carts) {
-        cardo.innerHTML = cardo.innerHTML + '<div onclick="turncard(this)" class="card"><div class="l1 face"><img src="./imagens/front 12.png"></div><div class="l2 back-face face"><img class=img src="./imagens/bobrossparrot.gif"></div></div>';
+function principal(){
+    const cardo = document.querySelector('.cards');
+    let carts=[];
+    carts.length = prompt('Quantas Cartas?');
+    let resto = carts.length % 2;
+    let acertos = 0;
+    let i = 0;
+    if (resto == 0 && carts.length > 2 && carts.length < 15) {
+    while (i < carts.length) {
+        cardo.innerHTML = cardo.innerHTML + '<li onclick="turncard(this)" class="card"> <div class="l1 face"><img src="./imagens/front 12.png"></div> <div class="l2 back-face face"><img class=img src="./imagens/bobrossparrot.gif"></div> </li>';
         i++;
     }
 }
-else { }
+else {principal()}
+
+}
